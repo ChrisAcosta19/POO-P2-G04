2339123b82eed160fc0a781715d9bbe055056fe9
Christopher Acosta <chriacos@espol.edu.ec>
2022-07-02 13:05:50 -0500

    Creacion de clase Servicio.java

---
+++ src/main/java/modelo/Servicio.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package modelo;
+
+/**
+ *
+ * @author chris
+ */
+public class Servicio {
+    private String nombre;
+    private int duracion;
+    private double precio;
+    private boolean estado; // false "Inactivo" true "Activo"
+
+    public Servicio(String nombre, int duracion, double precio) {
+        this.nombre = nombre;
+        this.duracion = duracion;
+        this.precio = precio;
+        this.estado = true;
+    }
+    
+    public Servicio(String nombre){
+        this.nombre = nombre;
+    }
+       
+    public void editarServicio(String nombre, int duracion, double precio, boolean estado){
+        this.nombre = nombre;
+        this.duracion = duracion;
+        this.precio = precio;
+        this.estado = estado;
+    }
+    
+    public void eliminarServicio(){
+        this.estado = false;
+    }
+    
+    public String toString(){
+        return "Nombre: "+nombre+ "\nDuracion: "+duracion
+                +"\nPrecio: "+precio+ "\nEstado: "+estado;
+    }    
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public boolean getEstado() {
+        return estado;
+    }
+    
+    @Override
+    public boolean equals(Object obj){
+        if(this == obj) {
+            return true;
+        }
+        if(obj != null && obj instanceof Servicio){
+            Servicio servicio = (Servicio) obj;
+            return nombre.equals(servicio.nombre);
+        }
+        return false;
+    }
+}
