a4f4ee3fc6df960eb1e941e3e37c8232dab6d222
Christopher Acosta <chriacos@espol.edu.ec>
2022-07-02 16:11:32 -0500

    Creacion de clase Atencion.java

--- src/main/java/modelo/Atencion.java
+++ src/main/java/modelo/Atencion.java
@@ -9,6 +9,7 @@ package modelo;
  * @author Christopher Acosta
  */
 public class Atencion {
+<<<<<<< HEAD
     private Cita cita;
     private int duracion;
     private Empleado empleadoAtencion;
@@ -36,4 +37,33 @@ public class Atencion {
         return "Cita registrada: "+cita.toString()+"\nduración: "+duracion+" minutos"
                 +", Empleado que atendió: "+empleadoAtencion.getNombre();
     }
+=======
+  private Cita cita;
+  private int duracion;
+  private Empleado empleadoAtencion;
+
+  public Atencion(Cita cita, int duracion, Empleado empleadoAtencion) {
+    this.cita = cita;
+    this.duracion = duracion;
+    this.empleadoAtencion = empleadoAtencion;
+  }
+    
+  @Override
+  public String toString(){
+    return "Cita registrada:\n"+cita.toString()+"\nduracion: "+duracion+
+            "Empleado que atendió:\n"+empleadoAtencion.toString();
+  }
+
+  public Cita getCita() {
+    return cita;
+  }
+  
+  public int getDuracion() {
+    return duracion;
+  }
+
+  public Empleado getEmpleadoCita() {
+    return empleadoAtencion;
+  }
+>>>>>>> fc06587 (Creacion de clase Atencion.java)
 }
