fcce61a2bb957868ff7bad96cfb69d1369e1377a
rgaibor17 <108085022+rgaibor17@users.noreply.github.com>
2022-07-02 16:50:01 -0500

    Add files via upload

---
+++ src/main/java/modelo/Persona.java
@@ -0,0 +1,56 @@
+package Modelo;
+
+/**
+ *
+ * @author Ronald Gaibor
+ */
+public class Persona {
+    private String cedula;
+    private String nombre;
+    private String telefono;
+    private String email;
+    
+    public Persona(String cedula, String nombre, String telefono, String email) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.telefono = telefono;
+        this.email = email;
+    }
+    
+    public String mostrarInformacion () {
+        return "Nombre: "+nombre+"\nCedula: "+cedula+"\nTelefono: "+telefono+"\nEmail: "+email;
+    }
+    
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
