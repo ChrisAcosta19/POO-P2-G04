6fad750e19d578a94ed42a6b1e32fb5188beb90b
Christopher Acosta <chriacos@espol.edu.ec>
2022-07-14 19:29:19 -0500

    Agregado metodo menuServicio en clase Menu

--- src/main/java/test/Menu.java
+++ src/main/java/test/Menu.java
@@ -68,6 +68,7 @@ public class Menu {
 >>>>>>> 64af89d (Actualización Menú Servicios)
     }
     
+<<<<<<< HEAD
     public static void mostrarMenuEmpleado(){
         System.out.println("Menú de Empleado:");
         System.out.println("1. Listar Empleados");
@@ -76,6 +77,74 @@ public class Menu {
         System.out.println("4. Eliminar Empleado");
         System.out.println("5. Salir al menú principal");
         System.out.println("Ingrese una opción: ");
+=======
+    public static void menuEmpleado(Scanner sc, Usuario usu){
+        String entradaDatos; Servicio servicio;
+        int opcion; Empleado empleado;
+        do{
+            System.out.println("Menú de Empleado:");
+            System.out.println("1. Listar Empleados");
+            System.out.println("2. Agregar Empleado");
+            System.out.println("3. Editar Empleado");
+            System.out.println("4. Eliminar Empleado");
+            System.out.println("5. Salir al menú principal");
+            do{
+                System.out.println("Ingrese una opción: ");
+                entradaDatos = sc.nextLine();
+            }while(!Validacion.validarEntero(entradaDatos));
+            opcion = Integer.parseInt(entradaDatos);
+            switch(opcion){
+                case 1:
+                    usu.listarEmpleados();
+                    break;
+                case 2:
+                    empleado = usu.crearEmpleado(sc, null);
+                    usu.empleados.add(empleado);
+                    break;
+                case 3:
+                    empleado = Main.buscarEmpleado(sc, usu);
+                    Empleado e = usu.crearEmpleado(sc, empleado);
+                    empleado.editarEmpleado(e);
+                do{
+                    do{
+                        System.out.println("Ingrese una de las opciones: ");
+                        System.out.println("1) Agregar servicio al empleado");
+                        System.out.println("2) Retirar servicio del empleado");
+                        System.out.println("3) No modificar los servicios del empleado");
+                        System.out.println("Ingrese su opción: ");
+                        entradaDatos = sc.nextLine();
+                    }while(!Validacion.validarEntero(entradaDatos));
+                    opcion = Integer.parseInt(entradaDatos);
+                    switch(opcion){
+                        case 1:
+                            servicio = Main.buscarServicio(sc, usu);
+                            empleado.getListaServicios().add(servicio);
+                            break;
+                        case 2:
+                            servicio = Main.buscarServicio(sc, usu);
+                            empleado.getListaServicios().remove(servicio);
+                            break;
+                        case 3:
+                            System.out.println("No se han modificado los servicios del empleado");
+                            break;
+                        default:
+                            System.out.println("Opción inválida, ingrese de nuevo\n");
+                    }
+                } while (opcion < 1 || opcion > 3);
+                break;
+                case 4:
+                    empleado = Main.buscarEmpleado(sc, usu);
+                    empleado.eliminarEmpleado();
+                    System.out.println("El empleado " + empleado.getNombre() + " ha sido cambiado a Inactivo");
+                    break;
+                case 5:
+                    System.out.println("Regresando al menú princial\n");
+                    break;
+                default:
+                    System.out.println("Opción inválida, ingrese de nuevo\n");
+                }
+        }while(opcion != 5);
+>>>>>>> c9a8aad (Agregado metodo menuServicio en clase Menu)
     }
     
     public static void mostrarMenuCliente(){
