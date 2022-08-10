f8258e09dc8b0c386a42554d7d7919f173459fcb
jppluas <jppluas@espol.edu.ec>
2022-08-10 01:41:24 -0500

    Creación de interfaz gráfica del menú principal

---
+++ src/main/java/gui/PrimaryController.java
@@ -0,0 +1,53 @@
+package gui;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+
+public class PrimaryController {
+
+    @FXML
+    private BorderPane mainPane;
+    @FXML
+    private Button btnSalir;
+    @FXML
+    private GridPane buttonsPane;
+    @FXML
+    private Button btnSrvs;
+    @FXML
+    private Button btnEmpl;
+    @FXML
+    private Button btnClients;
+    @FXML
+    private Button btnAtens;
+    @FXML
+    private Button btnCitas;
+
+    
+    @FXML
+    private void salir(ActionEvent event) {
+    }
+
+    @FXML
+    private void menuServicios(ActionEvent event) {
+    }
+
+    @FXML
+    private void menuEmpleados(ActionEvent event) {
+    }
+
+    @FXML
+    private void menuClientes(ActionEvent event) {
+    }
+
+    @FXML
+    private void menuAtenciones(ActionEvent event) {
+    }
+
+    @FXML
+    private void menuCitas(ActionEvent event) {
+    }
+}
---
+++ src/main/java/vista/files/PrimaryController.java
@@ -0,0 +1,11 @@
+
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PrimaryController {
+ 
+    @FXML private Button btnCitas
+    
+}
